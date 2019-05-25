aa0e1a837a16693b0eb418b7f873267a532ef1b2
Marvin Gaube <dev@marvingaube.de>
2019-05-25 23:38:31 +0200

    Start Broker and core-io

--- src/manager.ts
+++ src/manager.ts
@@ -1,2 +1,20 @@
+#!/usr/bin/env node
 // Import Utils
 import { Logging } from "@hibas123/nodelogging";
+
+// Import process stuff
+import child_process = require("child_process");
+
+// Run stuff
+Logging.log("ShowComposer is in startup");
+// Start Broker
+const processBroker = child_process.fork(require.resolve("@showcomposer/broker"));
+// Start IO
+const processIO = child_process.fork(require.resolve("@showcomposer/core-io"));
+
+// How to murder childs
+process.once("SIGINT", () => {
+  processIO.emit("SIGINT");
+  processBroker.emit("SIGINT");
+  Logging.log("ShowComposer shutting down");
+});
